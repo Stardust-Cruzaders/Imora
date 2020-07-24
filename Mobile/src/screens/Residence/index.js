d655fee97826bc043d655ef14154bd2025616043
Kauã Steliano <stelianok@gmail.com>
2020-07-24 10:52:47 -0300

    Img properties defined

--- Mobile/src/screens/Residence/index.js
+++ Mobile/src/screens/Residence/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {Text, View, useWindowDimensions, Image} from 'react-native';
 
@@ -5,15 +6,34 @@ import Icon from 'react-native-vector-icons/Feather';
 import styles from './styles';
 
 export default function Residence() {
+  const width = useWindowDimensions().width;
+  const height = useWindowDimensions().height;
+
   return (
     <View style={styles.container}>
-      <View style={styles.headerimgview} />
-      <View style={styles.basicInfoView} />
-      <View style={styles.ownerView} />
-      <View style={styles.descriptionView} />
-      <View styles={styles.comfortView} />
-      <View style={styles.conditionView} />
-      <View style={styles.proximities} />
+      <View style={[styles.headerImgView]}>
+        <Image
+          style={[
+            styles.mainImg,
+            {resizeMode: 'cover', width: width, height: height / 4},
+          ]}
+          source={{
+            uri: 'https://i.ytimg.com/vi/p-UOosKS8Ew/maxresdefault.jpg',
+          }}
+        />
+      </View>
+      <View style={[styles.bodyView, {width: width - 50}]}>
+        <View style={styles.basicInfoView}>
+          <Text style={styles.mainTitle}> Título </Text>
+          <Text style={styles.location}> Localização,Localização </Text>
+          <Text style={styles.price}> R$23,00/Mês </Text>
+        </View>
+        <View style={styles.ownerView} />
+        <View style={styles.descriptionView} />
+        <View styles={styles.comfortView} />
+        <View style={styles.conditionView} />
+        <View style={styles.proximities} />
+      </View>
     </View>
   );
 }
--- Mobile/src/screens/Residence/styles.js
+++ Mobile/src/screens/Residence/styles.js
@@ -1,10 +1,18 @@
-import React from 'react';
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF3',
+    backgroundColor: '#E8E8E8',
+
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  headerImgView: {
+    marginBottom: 5,
+  },
+  bodyView: {
+    backgroundColor: '#F9F8FD',
   },
 });
 
