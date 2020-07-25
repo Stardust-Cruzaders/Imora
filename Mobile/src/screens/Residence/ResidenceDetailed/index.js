f679d59494b13c987205fc4c61cb3fda29d14e79
Kauã Steliano <stelianok@gmail.com>
2020-07-25 20:14:58 -0300

    ImageSwipe component created, implemented ImageSwipe at ResidenceDetailed (#4)

--- Mobile/src/screens/Residence/ResidenceDetailed/index.js
+++ Mobile/src/screens/Residence/ResidenceDetailed/index.js
@@ -16,24 +16,25 @@ import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 import Div from '../../../Component/Div';
 
+import ImageSwipe from '../../../Component/ImageSwipe';
+
 export default function ResidenceDetailed() {
   const width = useWindowDimensions().width;
-  const height = useWindowDimensions().height;
+
+  const images = [
+    'https://i.pinimg.com/236x/09/66/4f/09664f3441de659f26bf604a2f1f8f43.jpg',
+    'https://i.pinimg.com/236x/1f/9a/44/1f9a4405c1db5d23a13d8608dfba6850.jpg',
+    'https://i.pinimg.com/236x/28/b9/43/28b94382c8bea2d56afbabe1369d3b68.jpg',
+    'https://i.pinimg.com/564x/9c/40/ac/9c40acb5931b72b8ace4cb446ee0d068.jpg',
+    'https://i.pinimg.com/236x/0d/a7/3b/0da73b6592ba04b63385c12280d1bf6a.jpg',
+  ];
 
   return (
     <ScrollView style={styles.scroll}>
+      <View style={[styles.headerImgView]}>
+        <ImageSwipe img={images} />
+      </View>
       <View style={styles.container}>
-        <View style={[styles.headerImgView]}>
-          <Image
-            style={[
-              styles.mainImg,
-              {resizeMode: 'cover', width: width, height: height / 4},
-            ]}
-            source={{
-              uri: 'https://i.ytimg.com/vi/p-UOosKS8Ew/maxresdefault.jpg',
-            }}
-          />
-        </View>
         <View style={[styles.bodyView, {width: width - 50}]}>
           <View style={styles.basicInfoView}>
             <Text style={styles.mainTitle}>Título </Text>
