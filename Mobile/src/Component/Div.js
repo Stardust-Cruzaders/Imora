cf7ef8d21adc5a6f5f3d7509b36cb4c45627c28b
Kauã Steliano <stelianok@gmail.com>
2020-07-25 11:52:17 -0300

    created Div component

---
+++ Mobile/src/Component/Div.js
@@ -0,0 +1,19 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+
+import {useWindowDimensions} from 'react-native';
+import {Divider} from 'react-native-elements';
+
+export default function Div({threshold}) {
+  const width = useWindowDimensions().width;
+  return (
+    <Divider
+      style={{
+        backgroundColor: '#3F3F3F',
+        width: width - threshold,
+        height: 1,
+        marginBottom: 25,
+      }}
+    />
+  );
+}
