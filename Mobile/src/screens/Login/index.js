0e6f18642e4d7fb781cbba6a615b64a7103bfd20
Kauã Steliano <stelianok@gmail.com>
2020-07-27 10:35:17 -0300

    Finished comfort screen design

--- Mobile/src/screens/Login/index.js
+++ Mobile/src/screens/Login/index.js
@@ -7,29 +7,23 @@ import Icon from 'react-native-vector-icons/Feather';
 Icon.loadFont();
 
 export default function Login() {
-  return(
+  return (
     <View style={styles.container}>
-        <View style={styles.headerAlign}>
-        <Icon name='home' size={50} color="#FFF"/>
+      <View style={styles.headerAlign}>
+        <Icon name="home" size={50} color="#FFF" />
         <Text style={styles.imoraText}>Imora</Text>
       </View>
 
-      <View style= {styles.whiteBox}>
-          <TouchableOpacity
-            style = {styles.feedButton}
-          >
-            <Text style ={styles.buttonText}>Explorar o App</Text>
-          </TouchableOpacity>   
-          <TouchableOpacity
-            style = {styles.facebookButton}
-          >
-            <Text style ={styles.buttonText}>Facebook</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style = {styles.googleButton}
-          >
-            <Text style ={styles.buttonText}>Google</Text>
-          </TouchableOpacity>
+      <View style={styles.whiteBox}>
+        <TouchableOpacity style={styles.feedButton}>
+          <Text style={styles.buttonText}>Explorar o App</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.facebookButton}>
+          <Text style={styles.buttonText}>Facebook</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.googleButton}>
+          <Text style={styles.buttonText}>Google</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
---
+++ Mobile/src/screens/Residence/ResidenceAdd/ResidenceAddComfort/styles.js
@@ -0,0 +1,69 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8E8E8',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    backgroundColor: '#7E57C2',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 25,
+    color: '#F9F8FD',
+
+    marginBottom: 15,
+
+    fontWeight: 'bold',
+  },
+  headerDivider: {
+    backgroundColor: '#F9F8FD',
+    height: 3,
+
+    alignSelf: 'center',
+  },
+  card: {
+    backgroundColor: '#FFF',
+
+    justifyContent: 'center',
+
+    borderRadius: 10,
+    padding: 20,
+  },
+  cardTitle: {
+    fontSize: 25,
+    alignSelf: 'center',
+
+    marginTop: 20,
+  },
+
+  description: {
+    color: '#8D8D97',
+    fontSize: 16,
+
+    left: 12,
+    marginBottom: 20,
+  },
+  checklist: {
+    marginHorizontal: 10,
+  },
+  cardFooter: {
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default styles;
