bff92f37fc1db8edd56a4a353de57ee9f6e523cf
Kauã Steliano <stelianok@gmail.com>
2020-07-30 15:20:26 -0300

    Finished Header from profileSelf

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 import {StatusBar} from 'react-native';
 
-import ResidenceEdit from './screens/Residence/ResidenceEdit';
+import ProfileSelf from './screens/Profile/ProfileSelf';
 
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor={'#4D2C91'} />
-      <ResidenceEdit />
+      <ProfileSelf />
     </>
   );
 }
