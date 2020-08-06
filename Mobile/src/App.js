859201a3fa63fd44b9858113cdf6f900c3dca4f2
Kauã Steliano <stelianok@gmail.com>
2020-08-06 15:45:13 -0300

    Bug at build.gradle fixed

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 
 import ProfileSelf from './screens/Profile/ProfileSelf';
-
+import Feed from './screens/Feed/';
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor={'#4D2C91'} />
-      <ProfileSelf />
+      <Feed />
     </>
   );
 }
