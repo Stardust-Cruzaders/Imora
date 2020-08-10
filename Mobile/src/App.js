4de10a116b7b61eafce7b080d4f8afad5288b13f
Kauã Steliano <stelianok@gmail.com>
2020-08-10 15:06:30 -0300

    Created Init.sql, with main tables.

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -4,6 +4,13 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 
 import ResidenceDetailed from './screens/Residence/ResidenceDetailed';
+
+import ResidenceAddComfort from './screens/Residence/ResidenceAdd/ResidenceAddComfort'
+import ResidenceAddConditions from './screens/Residence/ResidenceAdd/ResidenceAddConditions'
+import ResidenceAddLocation from './screens/Residence/ResidenceAdd/ResidenceAddLocation'
+import ResidenceAddMain from './screens/Residence/ResidenceAdd/ResidenceAddMain';
+import ResidenceAddType from './screens/Residence/ResidenceAdd/ResidenceAddType';
+import Feed from './screens/Feed';
 export default function App() {
   return (
     <>
