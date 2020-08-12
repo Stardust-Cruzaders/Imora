1368c670824ebe0e7f9d46300401764dea97f1e7
Kauã Steliano <stelianok@gmail.com>
2020-08-12 10:39:16 -0300

    Created GET Route

--- backend/src/controllers/ResidenceTypeController.js
+++ backend/src/controllers/ResidenceTypeController.js
@@ -3,9 +3,22 @@ const {pool} = require('../database/config');
 class ResidenceTypeController {
 
     async index(req,res){
-
+        pool.query('SELECT * FROM residencetype', (err, results) => {
+            if(err){
+                throw err;
+            }
+            else{
+                return res.status(200).send(results.rows);
+            }
+        });
     }
     async create(req,res){
+
+    }
+    async update(req,res){
+
+    }
+    async delete(req,res){
 
     }
 }
