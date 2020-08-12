3cd47015a983e630d2cd2415727ac1452386c6cf
Kauã Steliano <stelianok@gmail.com>
2020-08-12 08:56:34 -0300

    Controllers for auxiliar tables created

---
+++ backend/src/controllers/ResidencePlaceController.js
@@ -0,0 +1,12 @@
+import {pool} from '../database/config';
+
+class ResidencePlaceController {
+
+    async index(req,res){
+
+    }
+    async create(req,res){
+
+    }
+}
+module.exports = ResidencePlaceController;
---
+++ backend/src/controllers/ResidenceTypeController.js
@@ -0,0 +1,12 @@
+import {pool} from '../database/config';
+
+class ResidenceTypeController {
+
+    async index(req,res){
+
+    }
+    async create(req,res){
+
+    }
+}
+module.exports = ResidenceTypeController;
