01b5293c2fd45c193a8b0dc295f640168f2a5004
Kauã Steliano <stelianok@gmail.com>
2020-08-12 08:52:20 -0300

    Feature/add user route (#14)
    * Users are now added into the database, users can register with same email twice
    
    * Added some error messages for common errrors.

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -13,8 +13,27 @@ class UsersController {
 
     async create(request, response){
         const {Name, Email, Avatar,Description, IsHost, Phone} = request.body;
-        console.log(Name, Email, Avatar,Description, IsHost, Phone);
-        return response.status(201).send();
+        await pool.query('INSER INTO users(Name,Email,Avatar,Description,IsHost,Phone) VALUES($1,$2,$3,$4,$5,$6)', 
+        [Name,Email,Avatar,Description,IsHost, Phone], (error) => {
+            if(error){
+                if(error.code === '23505'){
+                    return response.status(400).send('A user with that email already exists');
+                }
+                else if(error.code === "42601") {
+                    return response.status(500).send("SQL syntax error");
+                }
+                else if(error.code === "42602"){
+                    return response.status(500).send("invalid_name Description: A table or database name has incorrect capitalization, characters or a mixture of both errors.");
+                }
+                else if(error.code === "42622"){
+                    return response.status(500).send("name_too_long Description: The identifier is longer than the 63-byte limit. This applies to names for databases, tables, schema, and other Postgres database object identifier names.");
+                }
+                else{
+                    throw error
+                }
+            }
+            response.status(201).json({status: 'success', message: 'User Added'});
+        })
     }
 }
 module.exports = UsersController;
