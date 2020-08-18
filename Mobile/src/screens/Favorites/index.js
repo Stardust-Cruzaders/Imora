4d276cf8e219e8b729cbb37cca89c4768c7859b4
Kauã Steliano <stelianok@gmail.com>
2020-08-18 19:43:35 -0300

    Created Basic Favorite Screen

--- Mobile/src/screens/Favorites/index.js
+++ Mobile/src/screens/Favorites/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 import styles from './styles';
-export default function Chat() {
+export default function Favorites() {
   return (
     <View style={styles.container}>
-      <Text>Chat</Text>
+      <Text>Favorites</Text>
     </View>
   );
 }
