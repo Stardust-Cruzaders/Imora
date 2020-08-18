02a3e4e6d2b695233224250be501ccf4f574234d
Kauã Steliano <stelianok@gmail.com>
2020-08-18 16:35:43 -0300

    Feature/main navigation (#23)
    * Updated some dependencies
    
    * Created Chat and Offers screens
    
    * formatted and removed unused stuff in the feed
    
    * Formatted the code  and removed unused components
    
    * Created Main Navigation

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -3,19 +3,12 @@ import React from 'react';
 
 import {StatusBar} from 'react-native';
 
-import ResidenceDetailed from './screens/Residence/ResidenceDetailed';
-
-import ResidenceAddComfort from './screens/Residence/ResidenceAdd/ResidenceAddComfort'
-import ResidenceAddConditions from './screens/Residence/ResidenceAdd/ResidenceAddConditions'
-import ResidenceAddLocation from './screens/Residence/ResidenceAdd/ResidenceAddLocation'
-import ResidenceAddMain from './screens/Residence/ResidenceAdd/ResidenceAddMain';
-import ResidenceAddType from './screens/Residence/ResidenceAdd/ResidenceAddType';
-import Feed from './screens/Feed';
+import MainNavigation from './routes/MainNavigation';
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor={'#4D2C91'} />
-      <Feed />
+      <MainNavigation />
     </>
   );
 }
--- Mobile/src/screens/Feed/index.js
+++ Mobile/src/screens/Feed/index.js
@@ -1,22 +1,13 @@
-import React, {useState} from 'react';
-import { View, SafeAreaView} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
 
 import styles from './styles';
-import Icon from 'react-native-vector-icons/Feather';
 import FeedBoxComponent from '../../Component/FeedBoxComponent';
-
 
 export default function Feed() {
   return (
     <SafeAreaView style={styles.container}>
       <FeedBoxComponent />
-
-      <View style={styles.bottomTabNav}>
-        <Icon name="compass" size={25} color="#FFF" />
-        <Icon name="zap" size={25} color="#FFF" />
-        <Icon name="message-circle" size={25} color="#FFF" />
-        <Icon name="user" size={25} color="#FFF" />
-      </View>
     </SafeAreaView>
   );
 }
