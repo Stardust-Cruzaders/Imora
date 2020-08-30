36c722db897119e9ddb4e89880cd895c12cf743f
Kauã Steliano <stelianok@gmail.com>
2020-08-30 16:13:40 -0300

    Added Navigators to App.js

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -5,18 +5,13 @@ import {StatusBar} from 'react-native';
 
 import ResidenceDetailed from './screens/Residence/ResidenceDetailed';
 
-import ResidenceAddComfort from './screens/Residence/ResidenceAdd/ResidenceAddComfort';
-import ResidenceAddConditions from './screens/Residence/ResidenceAdd/ResidenceAddConditions';
-import ResidenceAddLocation from './screens/Residence/ResidenceAdd/ResidenceAddLocation';
-import ResidenceAddMain from './screens/Residence/ResidenceAdd/ResidenceAddMain';
-import ResidenceAddType from './screens/Residence/ResidenceAdd/ResidenceAddType';
-import Feed from './screens/Feed';
-import FilterScreen from './screens/FilterScreen';
+import MainNavigation from './routes/MainNavigation';
+import ResidenceAddNavigation from './routes/ResidenceAddNavigation';
 export default function App() {
   return (
     <>
       <StatusBar backgroundColor={'#4D2C91'} />
-      <Feed />
+      <ResidenceAddNavigation />
     </>
   );
 }
