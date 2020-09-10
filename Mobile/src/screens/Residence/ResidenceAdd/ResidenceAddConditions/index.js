16ae60d042b9befe279baa6568c6f1c95cbda94f
Kauã Steliano <stelianok@gmail.com>
2020-09-10 17:13:39 -0300

    the user now can't leave important fields behind

--- Mobile/src/screens/Residence/ResidenceAdd/ResidenceAddConditions/index.js
+++ Mobile/src/screens/Residence/ResidenceAdd/ResidenceAddConditions/index.js
@@ -15,6 +15,7 @@ import {RadioButton, TextInput} from 'react-native-paper';
 import ResidenceAddHeader from '../../../../Component/ResidenceAddHeader';
 
 import {useResidenceAdd} from '../../../../contexts/residenceAdd';
+import {Root, Popup} from 'popup-ui';
 
 export default function ResidenceAddConditions({navigation}) {
   const {
@@ -29,145 +30,178 @@ export default function ResidenceAddConditions({navigation}) {
     setConditions,
   } = useResidenceAdd();
   const width = useWindowDimensions().width;
+
+  function VerifyFields() {
+    if (maxResidentNum != '' || maxResidentNum != '0') {
+      return true;
+    } else {
+      return false;
+    }
+  }
   return (
     <>
-      <ResidenceAddHeader />
-      <View style={styles.container}>
-        <View style={[styles.card, {width: width - 55}]}>
-          <Text style={[styles.cardTitle, textStyles.font]}> Condições </Text>
-          <Div threshold={120} height={2} />
-          <Text style={[styles.description, textStyles.font]}>
-            Aqui você pode definir diferentes tipos de restrições para sua
-            residência
-          </Text>
-          <View style={styles.checklist}>
-            <View
-              style={{
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'flex-start',
+      <Root>
+        <ResidenceAddHeader />
+        <View style={styles.container}>
+          <View style={[styles.card, {width: width - 55}]}>
+            <Text style={[styles.cardTitle, textStyles.font]}> Condições </Text>
+            <Div threshold={120} height={2} />
+            <Text style={[styles.description, textStyles.font]}>
+              Aqui você pode definir diferentes tipos de restrições para sua
+              residência
+            </Text>
+            <View style={styles.checklist}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'flex-start',
 
-                marginBottom: 20,
-              }}>
-              <Text style={[styles.cardText, textStyles.font]}>
-                Número máximo de ocupantes:
-              </Text>
-              <TextInput
-                style={[styles.input]}
-                value={maxResidentNum}
-                onChangeText={(text) => setMaxResidentNum(text)}
-                keyboardType={'number-pad'}
-                placeholder={'1'}
+                  marginBottom: 20,
+                }}>
+                <Text style={[styles.cardText, textStyles.font]}>
+                  Número máximo de ocupantes:
+                </Text>
+                <TextInput
+                  style={[styles.input]}
+                  value={maxResidentNum}
+                  onChangeText={(text) => setMaxResidentNum(text)}
+                  keyboardType={'number-pad'}
+                  placeholder={'1'}
+                />
+              </View>
+              <CheckboxComponent
+                value={allowPets}
+                setValue={setAllowPets}
+                text={'Animais de estimação dentro de casa são proibidos.'}
               />
-            </View>
-            <CheckboxComponent
-              value={allowPets}
-              setValue={setAllowPets}
-              text={'Animais de estimação dentro de casa são proibidos.'}
-            />
-            <CheckboxComponent
-              value={allowSmokers}
-              setValue={setAllowSmokers}
-              text={'Fumar dentro de casa é proibido.'}
-            />
-            <View style={styles.preferenceView}>
-              <Text
-                style={[
-                  styles.cardText,
-                  textStyles.font,
-                  {marginBottom: 10, marginTop: 25},
-                ]}>
-                Preferência de Residentes:{' '}
-              </Text>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <RadioButton
-                  value="Masculino"
-                  status={
-                    genderPreference === 'Masculino' ? 'checked' : 'unchecked'
-                  }
-                  onPress={() => setGenderPreference('Masculino')}
-                  color={'#7E57C2'}
-                />
-                <Text style={[styles.cardText, textStyles.font]}>
-                  Masculino
+              <CheckboxComponent
+                value={allowSmokers}
+                setValue={setAllowSmokers}
+                text={'Fumar dentro de casa é proibido.'}
+              />
+              <View style={styles.preferenceView}>
+                <Text
+                  style={[
+                    styles.cardText,
+                    textStyles.font,
+                    {marginBottom: 10, marginTop: 25},
+                  ]}>
+                  Preferência de Residentes:{' '}
                 </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <RadioButton
+                    value="Masculino"
+                    status={
+                      genderPreference === 'Masculino' ? 'checked' : 'unchecked'
+                    }
+                    onPress={() => setGenderPreference('Masculino')}
+                    color={'#7E57C2'}
+                  />
+                  <Text style={[styles.cardText, textStyles.font]}>
+                    Masculino
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <RadioButton
+                    value="Feminino"
+                    status={
+                      genderPreference === 'Feminino' ? 'checked' : 'unchecked'
+                    }
+                    onPress={() => setGenderPreference('Feminino')}
+                    color={'#7E57C2'}
+                  />
+                  <Text style={[styles.cardText, textStyles.font]}>
+                    Feminino
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <RadioButton
+                    value="Indiferente"
+                    status={
+                      genderPreference === 'Indiferente'
+                        ? 'checked'
+                        : 'unchecked'
+                    }
+                    onPress={() => setGenderPreference('Indiferente')}
+                    color={'#7E57C2'}
+                  />
+                  <Text style={[styles.cardText, textStyles.font]}>
+                    Indiferente
+                  </Text>
+                </View>
               </View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <RadioButton
-                  value="Feminino"
-                  status={
-                    genderPreference === 'Feminino' ? 'checked' : 'unchecked'
-                  }
-                  onPress={() => setGenderPreference('Feminino')}
-                  color={'#7E57C2'}
-                />
-                <Text style={[styles.cardText, textStyles.font]}>Feminino</Text>
+              <View style={styles.cardFooter}>
+                <BorderlessButton
+                  style={styles.button}
+                  onPress={() => {
+                    navigation.goBack();
+                  }}>
+                  <Icon
+                    name={'arrow-left-circle'}
+                    color={'#7E57C2'}
+                    size={40}
+                  />
+                </BorderlessButton>
+                <Text style={styles.dot}>•</Text>
+                <Text style={styles.dot}>•</Text>
+                <Text style={styles.dot}>•</Text>
+                <Text style={styles.activeDot}>•</Text>
+                <Text style={styles.dot}>•</Text>
+                <Text style={styles.dot}>•</Text>
+                <BorderlessButton
+                  style={styles.button}
+                  onPress={() => {
+                    setConditions(
+                      [
+                        {
+                          id: 'Animais de estimação',
+                          value: allowPets,
+                          icon: 'pets',
+                        },
+                        {
+                          id: 'Fumar dentro da residência',
+                          value: allowSmokers,
+                          icon: 'smoking-rooms',
+                        },
+                      ].filter((condition) => condition.value === true),
+                    );
+                    if (VerifyFields()) {
+                      navigation.navigate('ResidenceAddLocationZipcode');
+                    } else {
+                      Popup.show({
+                        type: 'Danger',
+                        title: 'Campos não preenchidos',
+                        button: true,
+                        textBody: 'Campos obrigatórios não foram preenchidos',
+                        buttontext: 'OK',
+                        callback: () => Popup.hide(),
+                      });
+                    }
+                  }}>
+                  <Icon
+                    name={'arrow-right-circle'}
+                    color={'#7E57C2'}
+                    size={40}
+                  />
+                </BorderlessButton>
               </View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}>
-                <RadioButton
-                  value="Indiferente"
-                  status={
-                    genderPreference === 'Indiferente' ? 'checked' : 'unchecked'
-                  }
-                  onPress={() => setGenderPreference('Indiferente')}
-                  color={'#7E57C2'}
-                />
-                <Text style={[styles.cardText, textStyles.font]}>
-                  Indiferente
-                </Text>
-              </View>
-            </View>
-            <View style={styles.cardFooter}>
-              <BorderlessButton
-                style={styles.button}
-                onPress={() => {
-                  navigation.goBack();
-                }}>
-                <Icon name={'arrow-left-circle'} color={'#7E57C2'} size={40} />
-              </BorderlessButton>
-              <Text style={styles.dot}>•</Text>
-              <Text style={styles.dot}>•</Text>
-              <Text style={styles.dot}>•</Text>
-              <Text style={styles.activeDot}>•</Text>
-              <Text style={styles.dot}>•</Text>
-              <Text style={styles.dot}>•</Text>
-              <BorderlessButton
-                style={styles.button}
-                onPress={() => {
-                  setConditions(
-                    [
-                      {
-                        id: 'Animais de estimação',
-                        value: allowPets,
-                        icon: 'pets',
-                      },
-                      {
-                        id: 'Fumar dentro da residência',
-                        value: allowSmokers,
-                        icon: 'smoking-rooms',
-                      },
-                    ].filter((condition) => condition.value === true),
-                  );
-                  navigation.navigate('ResidenceAddLocationZipcode');
-                }}>
-                <Icon name={'arrow-right-circle'} color={'#7E57C2'} size={40} />
-              </BorderlessButton>
             </View>
           </View>
         </View>
-      </View>
+      </Root>
     </>
   );
 }
--- Mobile/src/screens/Residence/ResidenceAdd/ResidenceAddLocation/ResidenceAddLocationAddress/index.js
+++ Mobile/src/screens/Residence/ResidenceAdd/ResidenceAddLocation/ResidenceAddLocationAddress/index.js
@@ -11,7 +11,7 @@ import ResidenceAddHeader from '../../../../../Component/ResidenceAddHeader';
 
 import {TextInput} from 'react-native-paper';
 import {useResidenceAdd} from '../../../../../contexts/residenceAdd';
-
+import {Root, Popup} from 'popup-ui';
 export default function ResidenceAddLocationAddress({navigation}) {
   const {
     street,
@@ -26,76 +26,105 @@ export default function ResidenceAddLocationAddress({navigation}) {
     setState,
   } = useResidenceAdd();
   const width = useWindowDimensions().width;
+  function VerifyFields() {
+    if (
+      street != '' &&
+      number != '' &&
+      neighborhood != '' &&
+      city != '' &&
+      state != ''
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   return (
     <>
-      <ResidenceAddHeader />
-      <View style={styles.container}>
-        <View style={[styles.card, {width: width - 55}]}>
-          <Text style={[styles.cardTitle, textStyles.font]}> Localização </Text>
-          <Div threshold={120} height={2} />
-          <Text style={styles.description}>
-            Aqui você pode definir a Localização da residência.
-          </Text>
-          <View style={styles.main}>
-            <TextInput
-              style={[styles.input, {width: width - 80}]}
-              value={street}
-              onChangeText={(text) => setStreet(text)}
-              placeholder={'Rua:'}
-            />
+      <Root>
+        <ResidenceAddHeader />
+        <View style={styles.container}>
+          <View style={[styles.card, {width: width - 55}]}>
+            <Text style={[styles.cardTitle, textStyles.font]}>
+              {' '}
+              Localização{' '}
+            </Text>
+            <Div threshold={120} height={2} />
+            <Text style={styles.description}>
+              Aqui você pode definir a Localização da residência.
+            </Text>
+            <View style={styles.main}>
+              <TextInput
+                style={[styles.input, {width: width - 80}]}
+                value={street}
+                onChangeText={(text) => setStreet(text)}
+                placeholder={'Rua:'}
+              />
 
-            <TextInput
-              style={[styles.input, {width: width - 80}]}
-              value={number}
-              onChangeText={(text) => setNumber(text)}
-              placeholder={'Número'}
-              keyboardType={'numeric'}
-            />
+              <TextInput
+                style={[styles.input, {width: width - 80}]}
+                value={number}
+                onChangeText={(text) => setNumber(text)}
+                placeholder={'Número'}
+                keyboardType={'numeric'}
+              />
 
-            <TextInput
-              style={[styles.input, {width: width - 80}]}
-              value={neighborhood}
-              onChangeText={(text) => setNeighborhood(text)}
-              placeholder={'Bairro'}
-            />
-            <TextInput
-              style={[styles.input, {width: width - 80}]}
-              value={city}
-              onChangeText={(text) => setCity(text)}
-              placeholder={'Cidade'}
-            />
-            <TextInput
-              style={[styles.input, {width: width - 80}]}
-              value={state}
-              onChangeText={(text) => setState(text)}
-              placeholder={'Estado'}
-            />
-          </View>
+              <TextInput
+                style={[styles.input, {width: width - 80}]}
+                value={neighborhood}
+                onChangeText={(text) => setNeighborhood(text)}
+                placeholder={'Bairro'}
+              />
+              <TextInput
+                style={[styles.input, {width: width - 80}]}
+                value={city}
+                onChangeText={(text) => setCity(text)}
+                placeholder={'Cidade'}
+              />
+              <TextInput
+                style={[styles.input, {width: width - 80}]}
+                value={state}
+                onChangeText={(text) => setState(text)}
+                placeholder={'Estado'}
+              />
+            </View>
 
-          <View style={styles.cardFooter}>
-            <BorderlessButton
-              style={styles.navButton}
-              onPress={() => {
-                navigation.goBack();
-              }}>
-              <Icon name={'arrow-left-circle'} color={'#7E57C2'} size={40} />
-            </BorderlessButton>
-            <Text style={styles.dot}>•</Text>
-            <Text style={styles.dot}>•</Text>
-            <Text style={styles.dot}>•</Text>
-            <Text style={styles.dot}>•</Text>
-            <Text style={styles.dot}>•</Text>
-            <Text style={styles.activeDot}>•</Text>
-            <BorderlessButton
-              style={styles.navButton}
-              onPress={() => {
-                navigation.navigate('ResidenceEdit');
-              }}>
-              <Icon name={'check-circle'} color={'#26E07C'} size={40} />
-            </BorderlessButton>
+            <View style={styles.cardFooter}>
+              <BorderlessButton
+                style={styles.navButton}
+                onPress={() => {
+                  navigation.goBack();
+                }}>
+                <Icon name={'arrow-left-circle'} color={'#7E57C2'} size={40} />
+              </BorderlessButton>
+              <Text style={styles.dot}>•</Text>
+              <Text style={styles.dot}>•</Text>
+              <Text style={styles.dot}>•</Text>
+              <Text style={styles.dot}>•</Text>
+              <Text style={styles.dot}>•</Text>
+              <Text style={styles.activeDot}>•</Text>
+              <BorderlessButton
+                style={styles.navButton}
+                onPress={() => {
+                  if (VerifyFields()) {
+                    navigation.navigate('ResidenceEdit');
+                  } else {
+                    Popup.show({
+                      type: 'Danger',
+                      title: 'Campos não preenchidos',
+                      button: true,
+                      textBody: 'Preencha todos os campos',
+                      buttontext: 'OK',
+                      callback: () => Popup.hide(),
+                    });
+                  }
+                }}>
+                <Icon name={'check-circle'} color={'#26E07C'} size={40} />
+              </BorderlessButton>
+            </View>
           </View>
         </View>
-      </View>
+      </Root>
     </>
   );
 }
