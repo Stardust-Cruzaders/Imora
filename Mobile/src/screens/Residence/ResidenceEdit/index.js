c95e7045c16d5aa0bf2242d91f60f2c56afd4ba7
Kauã Steliano <stelianok@gmail.com>
2020-09-12 14:22:26 -0300

    removed unused Array

--- Mobile/src/screens/Residence/ResidenceEdit/index.js
+++ Mobile/src/screens/Residence/ResidenceEdit/index.js
@@ -20,14 +20,6 @@ import {useResidenceAdd} from '../../../contexts/residenceAdd';
 export default function ResidenceEdit({navigation}) {
   const width = useWindowDimensions().width;
 
-  const images = [
-    'https://i.pinimg.com/236x/09/66/4f/09664f3441de659f26bf604a2f1f8f43.jpg',
-    'https://i.pinimg.com/236x/1f/9a/44/1f9a4405c1db5d23a13d8608dfba6850.jpg',
-    'https://i.pinimg.com/236x/28/b9/43/28b94382c8bea2d56afbabe1369d3b68.jpg',
-    'https://i.pinimg.com/564x/9c/40/ac/9c40acb5931b72b8ace4cb446ee0d068.jpg',
-    'https://i.pinimg.com/236x/0d/a7/3b/0da73b6592ba04b63385c12280d1bf6a.jpg',
-  ];
-
   const {
     title,
     price,
