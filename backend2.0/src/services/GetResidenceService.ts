b36d0227ba2e99dd0604435c17af7d68cee052d1
Kauã Steliano <stelianok@gmail.com>
2020-09-30 09:38:18 -0300

    Created route to get data about a specific house.

---
+++ backend2.0/src/services/GetResidenceService.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm';
+import Residence from '../models/Residence';
+
+import AppError from '../errors/AppError';
+import User from '../models/User';
+
+interface Request {
+  residence_id: string;
+}
+
+class GetResidenceService {
+  public async execute({ residence_id }: Request): Promise<Residence> {
+    const residenceRepository = getRepository(Residence);
+    const residence = await residenceRepository.findOne({
+      where: { id: residence_id },
+    });
+    if (!residence) {
+      throw new AppError('Residence not found.', 404);
+    }
+    const residenceAndOwnerData = await residenceRepository.query(
+      'SELECT residences.*, users.name, users.avatar FROM residences INNER JOIN users ON users.id = residences.owner_id WHERE residences.id = $1',
+      [residence_id],
+    );
+    if (!residenceAndOwnerData) {
+      throw new AppError('Residence and owner data not found');
+    }
+    return residenceAndOwnerData;
+  }
+}
+
+export default GetResidenceService;
