fdaaae9d91e27ef66ad662a55d52bebaceaaa7ff
Gabriel Ishibashi <gabriel-ishibashi@hotmail.com>
2020-10-01 21:11:06 -0300

    onBoarding v1, ele funciona mas ainda persiste o problema do primeiro slide

--- Mobile/src/App.js
+++ Mobile/src/App.js
@@ -12,11 +12,13 @@ import ResidenceAddMain from './screens/Residence/ResidenceAdd/ResidenceAddMain'
 import ResidenceAddType from './screens/Residence/ResidenceAdd/ResidenceAddType';
 import Feed from './screens/Feed';
 import FilterScreen from './screens/FilterScreen';
+import SwiperComponent from './screens/onBoarding';
+
 export default function App() {
     return (
         <>
             <StatusBar backgroundColor={'#4D2C91'} />
-            <Feed />
+            <SwiperComponent />
         </>
     );
 }
