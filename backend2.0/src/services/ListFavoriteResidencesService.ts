2a21f857d6d09e80aed0f1e81d48dbf9653e34ef
Kauã Steliano <stelianok@gmail.com>
2020-10-03 11:33:53 -0300

    Favorite listing now works 100%

--- backend2.0/src/services/ListFavoriteResidencesService.ts
+++ backend2.0/src/services/ListFavoriteResidencesService.ts
@@ -8,7 +8,7 @@ interface Request {
   id: string;
 }
 class ListFavoriteResidencesService {
-  public async execute({ id }: Request): Promise<Residence[]> {
+  public async execute({ id }: Request): Promise<Residence[] | string[]> {
     const residenceRepository = getRepository(Residence);
     const userRepository = getRepository(User);
 
@@ -16,13 +16,19 @@ class ListFavoriteResidencesService {
     if (!user) {
       throw new AppError("User doesn't exist");
     }
-
+    if (user.favorites.length < 1) {
+      return user.favorites;
+    }
     const favoriteResidences = await residenceRepository
       .createQueryBuilder('residences')
-      .innerJoinAndSelect('users', 'users', 'users.id = residences.owner_id')
+      .innerJoin('users', 'users', 'residences.owner_id = users.id')
+      .select(
+        'residences.*, users.name, users.email, users.avatar,users.bio, users.phone, users.is_host, users.user_city, users.user_state',
+      )
       .where({
         id: In(user.favorites),
       })
+      .andWhere('residences.available = true')
       .getRawMany();
 
     if (!favoriteResidences) {
