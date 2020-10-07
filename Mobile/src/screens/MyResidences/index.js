517807f286f7a4077a77897bc97c50515e620426
Kauã Steliano <stelianok@gmail.com>
2020-10-07 08:42:49 -0300

    Created MyResidences structure

--- Mobile/src/screens/MyResidences/index.js
+++ Mobile/src/screens/MyResidences/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, SafeAreaView, Text, ScrollView} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
+
+import Div from '../../Component/Div';
+import ResidenceToggle from '../../Component/ResidenceToggle';
 
 import styles from './styles';
 export default function MyResidences() {
   return (
-    <View style={styles.container}>
-      <Text>Minhas Residências</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.newResidenceView}>
+        <RectButton onPress={() => {}} style={styles.newResidencesButton}>
+          <Icon name={'home'} size={30} color={'#3f3f3f'} />
+          <Text style={styles.newResidencesButtonText}>
+            Criar novo anúncio{' '}
+          </Text>
+        </RectButton>
+      </View>
+      <Div threshold={100} />
+      <ScrollView>
+        <ResidenceToggle />
+        <ResidenceToggle />
+        <ResidenceToggle />
+        <ResidenceToggle />
+        <ResidenceToggle />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
