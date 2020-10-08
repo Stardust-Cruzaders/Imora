80cc1aefb3f75f6232ec0ab81f987f87b586af7b
Kauã Steliano <stelianok@gmail.com>
2020-10-08 17:08:08 -0300

    EditResidenceConfig basic stylization is done

--- Mobile/src/screens/EditResidenceConfig/index.js
+++ Mobile/src/screens/EditResidenceConfig/index.js
@@ -1,60 +1,91 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import {RectButton} from 'react-native-gesture-handler';
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Feather';
 import Div from '../../Component/Div';
-
 import styles from './styles';
 export default function EditResidenceConfig() {
   return (
     <View style={styles.container}>
       <View style={styles.card}>
         <View style={styles.section}>
-          <View style={styles.buttonView}>
-            <RectButton style={styles.button}>
-              <Text styles={styles.title}>
-                Disponibilidade:{' '}
-                <Text style={[styles.title, {color: '#26E07C'}]}>
-                  Disponível{' '}
-                </Text>
+          <View style={styles.infoView}>
+            <Text style={styles.title}>Disponibilidade: </Text>
+            <Text style={styles.status}>disponível</Text>
+            <View>
+              <RectButton style={styles.button}>
+                <Icon
+                  style={styles.powerIcon}
+                  name={'power'}
+                  size={40}
+                  color={'#26E07C'}
+                />
+              </RectButton>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.description}>
+              Se isso estiver desligado, a residência constará como ocupada e
+              não será mostrada aos usuários.
+            </Text>
+          </View>
+        </View>
+        <Div threshold={32} height={1.6} />
+        <RectButton>
+          <View style={styles.section}>
+            <View style={styles.headerView}>
+              <Text style={styles.title}>Alterar anúncio</Text>
+              <Icon
+                style={styles.icon}
+                name={'edit-3'}
+                size={28}
+                color={'#3f3f3f'}
+              />
+            </View>
+            <View>
+              <Text style={styles.description}>
+                Clique aqui para alterar as informações dessa residência.
               </Text>
-              <Icon name={'power'} size={28} color={'#26E07C'} />
-            </RectButton>
+            </View>
           </View>
-          <Text style={styles.description}>
-            Se isso estiver desligado, a residência constará como ocupada e não
-            será mostrada aos usuários.
-          </Text>
-        </View>
-        <Div threshold={100} />
-        <View style={styles.section}>
-          <View style={styles.buttonView}>
-            <RectButton style={styles.button}>
-              <Text styles={styles.title}>Alterar anúncio</Text>
-              <Icon name={'edit-3'} size={28} color={'#3f3f3f'} />
-            </RectButton>
+        </RectButton>
+        <Div threshold={32} height={1.5} />
+        <RectButton>
+          <View style={styles.section}>
+            <View style={styles.headerView}>
+              <Text style={styles.title}>Excluir anúncio</Text>
+              <Icon
+                style={styles.icon}
+                name={'trash-2'}
+                size={28}
+                color={'#3f3f3f'}
+              />
+            </View>
+            <View>
+              <Text style={styles.description}>
+                Clique aqui para excluir o seu anúncio permanentemente.
+              </Text>
+            </View>
           </View>
-          <Text style={styles.description}>
-            Clique aqui para alterar as informações dessa residência.
-          </Text>
-        </View>
-        <Div threshold={100} />
-        <View style={styles.section}>
-          <View style={styles.buttonView}>
-            <RectButton style={styles.button}>
-              <Text styles={styles.title}>Excluir anúncio</Text>
-              <Icon name={'trash-2'} size={28} color={'#3f3f3f'} />
-            </RectButton>
+        </RectButton>
+        <Div threshold={32} height={1.5} />
+        <View>
+          <View style={styles.section}>
+            <View style={styles.headerView}>
+              <Text style={styles.title}>Interessados</Text>
+              <Icon
+                style={styles.icon}
+                name={'edit-3'}
+                size={28}
+                color={'#3f3f3f'}
+              />
+            </View>
+            <View>
+              <Text style={styles.description}>
+                Clique aqui para alterar as informações dessa residência.
+              </Text>
+            </View>
           </View>
-          <Text style={styles.description}>
-            Essa opção excluirá seu anúncio permanentemente.
-          </Text>
-        </View>
-        <Div threshold={100} />
-        <View style={styles.section}>
-          <Text styles={styles.title}>Residentes</Text>
-
-          <Text style={styles.description}>Nenhum</Text>
         </View>
       </View>
     </View>
--- Mobile/src/screens/EditResidenceConfig/styles.js
+++ Mobile/src/screens/EditResidenceConfig/styles.js
@@ -1,12 +1,59 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
-  card: {},
-  section: {},
-  buttonView: {},
-  button: {},
-  title: {},
-  description: {},
+  container: {
+    flex: 1,
+
+    backgroundColor: '#E8E8E8',
+  },
+  card: {
+    backgroundColor: '#FFF',
+    alignSelf: 'center',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    marginHorizontal: 25,
+  },
+  section: {
+    margin: 15,
+  },
+  headerView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 5,
+  },
+  infoView: {
+    flexDirection: 'row',
+  },
+  title: {
+    color: '#3F3F3F',
+
+    fontSize: 20,
+
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+  },
+  status: {
+    color: '#8D8D97',
+
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    marginRight: 90,
+  },
+  icon: {
+    alignSelf: 'center',
+  },
+  powerIcon: {
+    alignSelf: 'center',
+    padding: 5,
+  },
+  description: {
+    color: '#8D8D97',
+
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    textAlign: 'left',
+  },
 });
 export default styles;
