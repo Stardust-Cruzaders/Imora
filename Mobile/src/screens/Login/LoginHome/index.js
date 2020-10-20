af67447f246844d31d1a5a5db4ca2ffcb940b1b4
Kauã Steliano <stelianok@gmail.com>
2020-10-20 17:02:20 -0300

    updated some dependencies, formatted LoginHome.

--- Mobile/src/screens/Login/LoginHome/index.js
+++ Mobile/src/screens/Login/LoginHome/index.js
@@ -5,47 +5,51 @@ import {RectButton} from 'react-native-gesture-handler';
 
 import styles from './styles';
 
-export default function LoginHome(){
-    return(
+export default function LoginHome() {
+  return (
     <View style={styles.container}>
-        <ImageBackground 
-        source={{uri:'https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60'}}
+      <ImageBackground
+        source={{
+          uri:
+            'https://images.unsplash.com/photo-1514924013411-cbf25faa35bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+        }}
         style={styles.imageBackground}
-        imageStyle={{opacity: 0.3}}
-        > 
-        <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 60}}>
-        <Text style={styles.fontTitle}>Bem vindo de volta!</Text>
-        
-        <View style={styles.whiteBox}>
+        imageStyle={{opacity: 0.3}}>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 60,
+          }}>
+          <Text style={styles.fontTitle}>Bem vindo de volta!</Text>
+
+          <View style={styles.whiteBox}>
             <View style={{flexDirection: 'row'}}>
-            <Icon name="mail" size={25} color="#7E57C2"/>
-            <TextInput 
-            style={{height: 20, borderBottomWidth: 1}} 
-            placeholder="Email" 
-            placeholderTextColor= '#fff' 
-            />
+              <Icon name="mail" size={25} color="#7E57C2" />
+              <TextInput
+                style={{height: 20, borderBottomWidth: 1}}
+                placeholder="Email"
+                placeholderTextColor="#fff"
+              />
             </View>
-           
+
             <View style={{flexDirection: 'row'}}>
-            <Icon name="lock" size={25} color="#7E57C2"/>
-            <TextInput 
-            style={{height: 20, borderBottomWidth: 1}} 
-            placeholder="Senha" 
-            placeholderTextColor="gray" />
+              <Icon name="lock" size={25} color="#7E57C2" />
+              <TextInput
+                style={{height: 20, borderBottomWidth: 1}}
+                placeholder="Senha"
+                placeholderTextColor="gray"
+              />
             </View>
-          
+
             <RectButton style={styles.buttonStyle}>
-                <Text style={styles.textButton}>Continuar</Text>
+              <Text style={styles.textButton}>Continuar</Text>
             </RectButton>
+          </View>
+          <Text style={styles.subTextWhite}>Esqueceu sua senha?</Text>
+          <Text style={styles.subTextGreen}>Recupere-a aqui</Text>
         </View>
-        <Text style={styles.subTextWhite}>Esqueceu sua senha?</Text>
-        <Text style={styles.subTextGreen}>Recupere-a aqui</Text>
-    
-        
-        </View>
-        
-          
-          </ImageBackground>
+      </ImageBackground>
     </View>
-    );
+  );
 }
