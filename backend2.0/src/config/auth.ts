210b28b1d4f6b0e3f3192afc221db80d5f3460a1
Kauã Steliano <stelianok@gmail.com>
2020-10-29 09:40:51 -0300

    Removed backend

--- backend2.0/src/config/auth.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-export default {
-  jwt: {
-    secret: process.env.APP_SECRET,
-    expiresIn: '1d',
-  },
-};
--- backend2.0/src/repositories/ResidenceRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-
-import Residence from '../models/Residence';
-
-@EntityRepository(Residence)
-class ResidenceRepository extends Repository<Residence> {}
-export default ResidenceRepository;
