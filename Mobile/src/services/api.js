91a8c51306a82fd38f31ac76510cbba9ace84c19
Kauã Steliano <stelianok@gmail.com>
2020-11-03 09:06:02 -0300

    Changed base URL

--- Mobile/src/services/api.js
+++ Mobile/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.14:3333',
+  baseURL: 'https://imora-rest-api.herokuapp.com',
+  //Localhost: 'http://192.168.15.14:3333',
 });
 export default api;
