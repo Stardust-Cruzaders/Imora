3d28153c3eaf6ce9fa9e2ba1bfeb768072a84669
Kauã Steliano <stelianok@gmail.com>
2020-11-06 10:07:46 -0300

    Created List of old pictures

--- Mobile/src/screens/Residence/ResidenceEditPhotos/index.js
+++ Mobile/src/screens/Residence/ResidenceEditPhotos/index.js
@@ -1,11 +1,162 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import {nanoid} from 'nanoid';
+import React, {useState, useEffect} from 'react';
+import {Alert, Text, View, FlatList, Image} from 'react-native';
+import {BorderlessButton, RectButton} from 'react-native-gesture-handler';
+import ImagePicker from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/Feather';
 
+import ResidenceAddHeader from '../../../Component/ResidenceAddHeader';
 import styles from './styles';
-export default function ResidenceEditPhotos() {
+export default function ResidenceEditPhotos({navigation, route}) {
+  const [newImagesArray, setNewImagesArray] = useState([]);
+  const [newImagesObject, setNewImagesObject] = useState([]);
+  const [oldImagesArray, setOldImagesArray] = useState([]);
+
+  const [imagesToDelete, setImagesToDelete] = useState([]);
+  const [imagesToAdd, setImagesToAdd] = useState([]);
+  const {residence} = route.params;
+  useEffect(() => {
+    setOldImagesArray(residence.images);
+  }, [residence.images]);
+  const selectFile = () => {
+    var options = {
+      title: 'Selecionar imagem',
+      takePhotoButtonTitle: 'Tirar uma foto',
+      chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      storageOptions: {
+        skipBackup: true,
+        path: 'imora',
+      },
+    };
+
+    ImagePicker.showImagePicker(options, (res) => {
+      if (res.didCancel) {
+      } else if (res.error) {
+      } else if (res.customButton) {
+        alert(res.customButton);
+      } else {
+        const image = {
+          uri: res.uri,
+          name: res.fileName,
+          type: res.type,
+        };
+        setNewImagesObject([...newImagesObject, image]);
+        setOldImagesArray([...oldImagesArray, res]);
+      }
+    });
+  };
+
+  function DeleteOldImg(item) {
+    setOldImagesArray(
+      oldImagesArray.filter((img) => {
+        return img !== item;
+      }),
+    );
+
+    setImagesToDelete([
+      ...imagesToDelete,
+      oldImagesArray.filter((img) => {
+        return img === item;
+      }),
+    ]);
+  }
+
+  const DeleteImageConfirmation = (item, imageType) => {
+    Alert.alert(
+      'Confirmação de exclusão',
+      'Você deseja excluir essa imagem?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel',
+        },
+        {
+          text: 'Sim',
+          onPress: () => {
+            if (imageType === 'old') {
+              DeleteOldImg(item);
+            } else if (imageType === 'new') {
+            }
+          },
+          style: 'destructive',
+        },
+      ],
+      {cancelable: false},
+    );
+  };
+
   return (
-    <View styles={styles.container}>
-      <Text>assustei kk</Text>
-    </View>
+    <>
+      <ResidenceAddHeader
+        title={'Edite suas fotos'}
+        subtitle={'Clique em uma foto pra excluí-la.'}
+      />
+      <View styles={styles.container}>
+        <View style={styles.oldPhotosView}>
+          <Text style={styles.oldImagesTitle}>Fotos atuais: </Text>
+          <FlatList
+            data={oldImagesArray}
+            keyExtractor={() => nanoid(9)}
+            numColumns={3}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.imageComponent}>
+                  <BorderlessButton
+                    style={styles.deleteImageButton}
+                    onPress={() => {
+                      DeleteImageConfirmation(item);
+                    }}>
+                    <Image
+                      source={{
+                        uri: item,
+                      }}
+                      style={styles.oldImg}
+                    />
+                  </BorderlessButton>
+                </View>
+              );
+            }}
+          />
+        </View>
+        <View style={styles.oldPhotosView}>
+          <Text style={styles.oldImagesTitle}>
+            Fotos que serão adicionadas:
+          </Text>
+          <FlatList
+            data={newImagesArray}
+            keyExtractor={() => nanoid(9)}
+            numColumns={3}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.imageComponent}>
+                  <BorderlessButton
+                    style={styles.deleteImageButton}
+                    onPress={() => {
+                      DeleteImageConfirmation(item);
+                    }}>
+                    <Image
+                      source={{
+                        uri: item,
+                      }}
+                      style={styles.oldImg}
+                    />
+                  </BorderlessButton>
+                </View>
+              );
+            }}
+          />
+          <RectButton style={styles.addImageButton} onPress={() => {}}>
+            <View style={styles.iconView}>
+              <Icon name={'paperclip'} color={'#3F3F3F'} size={24} />
+              <Text style={styles.addImageButtonText}>Adicionar Foto </Text>
+              <Icon name={'chevron-right'} color={'#3F3F3F'} size={24} />
+            </View>
+          </RectButton>
+        </View>
+        <RectButton style={styles.saveButton} onPress={() => {}}>
+          <Text>Salvar Modificações</Text>
+        </RectButton>
+      </View>
+    </>
   );
 }
--- Mobile/src/screens/Residence/ResidenceEditPhotos/styles.js
+++ Mobile/src/screens/Residence/ResidenceEditPhotos/styles.js
@@ -1,6 +1,55 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  imageComponent: {
+    marginHorizontal: 5,
+  },
+  imageView: {
+    flexDirection: 'row',
+  },
+  deleteImageButton: {
+    width: 125,
+    height: 125,
+    marginVertical: 5,
+  },
+  oldImagesTitle: {
+    fontFamily: 'Roboto',
+    fontSize: 25,
+    marginVertical: 15,
+    marginLeft: 5,
+    color: 'black',
+  },
+  oldImg: {
+    width: 125,
+    height: 125,
+    resizeMode: 'cover',
+  },
+  addImageButton: {
+    marginVertical: 25,
+    backgroundColor: '#DDE0E3',
+
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    alignSelf: 'center',
+    alignContent: 'center',
+    padding: 10,
+
+    borderRadius: 6,
+
+    paddingBottom: 10,
+  },
+  addImageButtonText: {
+    color: '#3F3F3F',
+    fontSize: 20,
+    fontFamily: 'Roboto',
+    marginLeft: 5,
+  },
+  iconView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 export default styles;
